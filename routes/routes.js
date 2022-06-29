5e13fd75fb62102ca14d93fe345c336232a0c3ab
Khenay <jurjomed@hotmail.com>
2022-06-29 17:46:27 +0200

    registro y login

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+// const user = require("../controllers/user.controllers");
+
+
+// router.get("/",user.home)
+router.post("/register");
+// router.get("/confirmuser/:id/:token", user.confirmUserGet);
+// router.post("/confirmuser/:id/:token", user.checkUserPost);
+// router.get("/datauser", user.dataUser);
+
+module.exports = router;
